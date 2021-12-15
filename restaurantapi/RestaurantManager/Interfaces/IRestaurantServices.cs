fc445d0cb2ed7760f4b12a0f8921310ac20d1b36
Jesus Juarez <jss9960@hotmail.com>
2021-12-15 00:33:38 -0800

    Se agregro manager y se agrego interface para consumir la lista de todos los restaurantes, todavia esta en prototipo

---
+++ restaurantapi/RestaurantManager/Interfaces/IRestaurantServices.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantManager.Interfaces
+{
+    public interface IRestaurantServices
+    {
+        public void GetAllAvailableRestaurants();
+    }
+}
---
+++ restaurantapi/RestaurantManager/Services/RestaurantServices.cs
@@ -0,0 +1,15 @@
+﻿using RestaurantManager.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantManager.Services
+{
+    public class RestaurantServices : IRestaurantServices
+    {
+        public void GetAllAvailableRestaurants()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
