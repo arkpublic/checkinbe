8a740740b19277a1e57b05839a688c2a40e5f61e
nthony <jesus.juarez.dev@gmail.com>
2022-07-07 00:09:34 -0700

    Added vue bootstrap

--- restaurantapi/RestaurantCrm/src/components/Home.vue
+++ restaurantapi/RestaurantCrm/src/components/Home.vue
@@ -1,20 +1,50 @@
 <template>
-    <div class="home">
-        <h1>{{ msg }}</h1>
-        <p>Welcome to your new single-page application, built with <a href="https://vuejs.org" target="_blank">Vue.js</a>.</p>
+    <div>
+        <b-alert show>Default Alert</b-alert>
+
+        <b-alert variant="success" show>Success Alert</b-alert>
+
+        <b-alert v-model="showDismissibleAlert" variant="danger" dismissible>
+            Dismissible Alert!
+        </b-alert>
+
+        <b-alert :show="dismissCountDown"
+                 dismissible
+                 variant="warning"
+                 @dismissed="dismissCountDown=0"
+                 @dismiss-count-down="countDownChanged">
+            <p>This alert will dismiss after {{ dismissCountDown }} seconds...</p>
+            <b-progress variant="warning"
+                        :max="dismissSecs"
+                        :value="dismissCountDown"
+                        height="4px"></b-progress>
+        </b-alert>
+
+        <b-button @click="showAlert" variant="info" class="m-1">
+            Show alert with count-down timer
+        </b-button>
+        <b-button @click="showDismissibleAlert=true" variant="info" class="m-1">
+            Show dismissible alert ({{ showDismissibleAlert ? 'visible' : 'hidden' }})
+        </b-button>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'Home',
-        props: {
-            msg: String
+        data() {
+            return {
+                dismissSecs: 10,
+                dismissCountDown: 0,
+                showDismissibleAlert: false
+            }
+        },
+        methods: {
+            countDownChanged(dismissCountDown) {
+                this.dismissCountDown = dismissCountDown
+            },
+            showAlert() {
+                this.dismissCountDown = this.dismissSecs
+            }
         }
-    };
+    }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
-
