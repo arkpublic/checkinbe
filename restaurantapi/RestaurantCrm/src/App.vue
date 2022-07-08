c01c79f42aba41972b070fc146238ff80d67de21
nthony <jesus.juarez.dev@gmail.com>
2022-07-08 00:59:57 -0700

    Added custom header

--- restaurantapi/RestaurantCrm/src/App.vue
+++ restaurantapi/RestaurantCrm/src/App.vue
@@ -1,18 +1,23 @@
 <template>
     <div id="app">
+        <CustomHeader></CustomHeader>
         <Home msg="Hello world!" />
     </div>
 </template>
 
 <script>
     import Home from './components/Home.vue';
+    import CustomHeader from './components/shared/CustomHeader.vue';
 
     export default {
         name: 'app',
         components: {
-            Home
+            Home,
+            CustomHeader
         }
     };
+
+
 </script>
 
 <style>
---
+++ restaurantapi/RestaurantCrm/src/components/shared/CustomHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <b-navbar type="dark" variant="dark">
+            <b-navbar-nav>
+                <b-nav-item href="#">Home</b-nav-item>
+
+                <b-nav-item-dropdown text="Services" right>
+                    <b-dropdown-item href="#">Restaurants</b-dropdown-item>
+                    <b-dropdown-item href="#">Menus</b-dropdown-item>
+                    <b-dropdown-item href="#">Users</b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-navbar-nav>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
